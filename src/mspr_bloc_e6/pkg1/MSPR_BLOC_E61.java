4b9f67e0024d95260239417fe253be247c9f4d15
Nicolas De Jesus <ndejesus34800@gmail.com>
2022-02-28 09:58:55 +0100

    init

---
+++ src/mspr_bloc_e6/pkg1/MSPR_BLOC_E61.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mspr_bloc_e6.pkg1;
+
+/**
+ *
+ * @author ndeje
+ */
+public class MSPR_BLOC_E61 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
